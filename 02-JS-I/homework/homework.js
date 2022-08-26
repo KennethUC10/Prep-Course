4c7961b2ec6dfbaf490e4ade8c11578dcb45ea29
KUC <Kenneth.urbina.x@gmail.com>
2022-08-26 19:38:45 -0400

    update 02-JS-I/homework.js file

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 25;
+const nuevoNum = 10;
 
 // Crea una variable booleana:
 const nuevoBool = true;
